3d42afefa040758b6249b158b0eee09175f818cd
Khaled Mitkees <Khaled.mitkees@gmail.com>
2023-11-27 23:39:55 +0200

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Khaled Mitkees on 27/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
