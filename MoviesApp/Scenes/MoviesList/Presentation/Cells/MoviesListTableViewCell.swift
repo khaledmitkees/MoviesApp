6de81320373fdf77bd85b4c906849b74f63121c5
Khaled Mitkees <Khaled.mitkees@gmail.com>
2023-11-28 13:11:23 +0200

    create movies presentation layer

---
+++ MoviesApp/Scenes/MoviesList/Presentation/Cells/MoviesListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MoviesListTableViewCell.swift
+//  MoviesApp
+//
+//  Created by Khaled Mitkees on 28/11/2023.
+//
+
+import UIKit
+
+class MoviesListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MoviesApp/Scenes/MoviesList/Presentation/ViewModel/MoviesListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesListViewModel.swift
+//  MoviesApp
+//
+//  Created by Khaled Mitkees on 28/11/2023.
+//
+
+import Foundation
+
+protocol MoviesListViewModelContract {
+    func getMovies()
+}
+
+final class MoviesListViewModel: MoviesListViewModelContract {
+    func getMovies() {
+        
+    }
+}
