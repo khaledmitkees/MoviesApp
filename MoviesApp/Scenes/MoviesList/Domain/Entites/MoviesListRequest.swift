d10343db56b0c81b9fbe339aa9187fb7ba3b4421
Khaled Mitkees <Khaled.mitkees@gmail.com>
2023-11-29 22:59:08 +0200

    created movies list request dto

---
+++ MoviesApp/Scenes/MoviesList/Domain/Entites/MoviesListRequest.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesListRequest.swift
+//  MoviesApp
+//
+//  Created by Khaled Mitkees on 29/11/2023.
+//
+
+import Foundation
+
+struct MoviesListRequest: Encodable {
+    let page: Int
+}
