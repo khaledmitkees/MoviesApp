d76cfcf1e8c66140119e363b82ef2f7cfc806d9b
Khaled Mitkees <Khaled.mitkees@gmail.com>
2023-11-29 18:37:10 +0200

    added app configuration

---
+++ MoviesApp/Application/AppConfiguration.swift
@@ -0,0 +1,23 @@
+//
+//  AppConfiguration.swift
+//  MoviesApp
+//
+//  Created by Khaled Mitkees on 29/11/2023.
+//
+
+import Foundation
+
+final class AppConfiguration {
+    lazy var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey must not be empty in plist")
+        }
+        return apiKey
+    }()
+    lazy var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+}
