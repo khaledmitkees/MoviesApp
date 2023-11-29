f439d4f504466ead441a99bd8ff6b5cc463b4db3
Khaled Mitkees <Khaled.mitkees@gmail.com>
2023-11-29 16:12:34 +0200

    add network logger

---
+++ MoviesApp/Infrastructure/Logger/NetworkLogger.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkLogger.swift
+//  MoviesApp
+//
+//  Created by Khaled Mitkees on 28/11/2023.
+//
+
+import Foundation
+
+protocol NetworkLoggerContract {
+    func log(request: URLRequest)
+    func log(responseData data: Data?, response: URLResponse?)
+    func log(error: Error)
+}
+
+final class NetworkLogger: NetworkLoggerContract {
+    func log(request: URLRequest) {
+        printIfDebug("-------------")
+        printIfDebug("request: \(request.url!)")
+        printIfDebug("headers: \(request.allHTTPHeaderFields!)")
+        printIfDebug("method: \(request.httpMethod!)")
+        if let httpBody = request.httpBody, let result = ((try? JSONSerialization.jsonObject(with: httpBody, options: []) as? [String: AnyObject]) as [String: AnyObject]??) {
+            printIfDebug("body: \(String(describing: result))")
+        } else if let httpBody = request.httpBody, let resultString = String(data: httpBody, encoding: .utf8) {
+            printIfDebug("body: \(String(describing: resultString))")
+        }
+    }
+
+    func log(responseData data: Data?, response: URLResponse?) {
+        guard let data = data else { return }
+        if let dataDict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            printIfDebug("responseData: \(String(describing: dataDict))")
+        }
+    }
+
+    func log(error: Error) {
+        printIfDebug("\(error)")
+    }
+    
+    private func printIfDebug(_ string: String) {
+        #if DEBUG
+        print(string)
+        #endif
+    }
+}
