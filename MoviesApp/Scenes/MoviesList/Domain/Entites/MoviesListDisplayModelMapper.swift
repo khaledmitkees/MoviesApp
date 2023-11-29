398b3267d2bf0e871b574f75170b628fe4fb05f2
Khaled Mitkees <Khaled.mitkees@gmail.com>
2023-11-30 01:10:26 +0200

    added display model

---
+++ MoviesApp/Scenes/MoviesList/Domain/Entites/MoviesListDisplayModelMapper.swift
@@ -0,0 +1,14 @@
+//
+//  MoviesListDisplayModelMapper.swift
+//  MoviesApp
+//
+//  Created by Khaled Mitkees on 30/11/2023.
+//
+
+import Foundation
+
+struct MoviesListDisplayModel {
+    let title: String
+    let overview: String
+    let posterPath: String
+}
