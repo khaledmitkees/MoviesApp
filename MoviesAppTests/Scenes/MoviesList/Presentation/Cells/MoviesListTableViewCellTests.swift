b63463776422bfddf7292e01ce4ea4636bae8151
Khaled Mitkees <Khaled.mitkees@gmail.com>
2023-11-30 02:27:43 +0200

    added tableview cell tests

---
+++ MoviesAppTests/Scenes/MoviesList/Presentation/Cells/MoviesListTableViewCellTests.swift
@@ -0,0 +1,39 @@
+//
+//  MoviesListTableViewCellTests.swift
+//  MoviesAppTests
+//
+//  Created by Khaled Mitkees on 30/11/2023.
+//
+
+import XCTest
+
+class MoviesListTableViewCellTests: XCTestCase {
+    
+    func testConfigure() {
+        // Arrange
+        let cell = MoviesListTableViewCell()
+        
+        // Simulate the creation of outlets
+        let movieTitle = UILabel()
+        let movieOverView = UILabel()
+        let movieImageView = UIImageView()
+
+        cell.movieTitle = movieTitle
+        cell.movieOverView = movieOverView
+        cell.movieImageView = movieImageView
+
+        let mockMovie = MoviesListDisplayModel(
+            id: 1,
+            title: "Mock Movie",
+            overview: "Mock Overview",
+            posterPath: "/mockPosterPath.jpg"
+        )
+
+        // Act
+        cell.configure(with: mockMovie)
+
+        // Assert
+        XCTAssertEqual(cell.movieTitle?.text, "Mock Movie")
+        XCTAssertEqual(cell.movieOverView?.text, "Mock Overview")
+    }
+}
